987828b76408392e54dba103b117e7b04d1bcd93
bhagyashribadave22 <117337617+bhagyashribadave22@users.noreply.github.com>
2022-11-08 16:21:21 +0530

    Update home.js

--- Day3/home.js
+++ Day3/home.js
@@ -2,12 +2,20 @@ import React from "react"
 import Navbar from "../../component/navbar/navar"
 
 
-
 const Home=()=>{
   return(
     <div className="Home-container">
       <div className="Home-wrapper">
      <Navbar/>
+     <h1>Fun facts about React</h1>
+      <br></br>
+      
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by jorden walke</li>
+        <li>Has well over 100k strts on GitHub</li>
+      </ul>
+     
       </div>
     </div>
   )
